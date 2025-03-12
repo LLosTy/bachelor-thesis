bc235ecd0c4010b5a4097066ab0872224051445e
LLosTy <marcojurgenjuras@gmail.com>
2025-03-12 21:25:50 +0100

    fix(CarListings.js): fixed missing thumbnail when using filter component

--- components/CarListings.jsx
+++ components/CarListings.jsx
@@ -11,15 +11,28 @@ export const CarListings = ({ cars, formatPrice, formatMileage }) => {
 
   return (
     <div className="row g-4">
-      {cars.map((car) => (
-        <div key={car.id} className="col-md-6 col-lg-4">
-          <CarCard
-            car={car}
-            formatPrice={formatPrice}
-            formatMileage={formatMileage}
-          />
-        </div>
-      ))}
+      {cars.map((car) => {
+        // Create a processed car object that works with both data sources
+        const processedCar = {
+          ...car,
+          // Keep existing thumbnail if it exists, otherwise extract from images_id
+          thumbnail:
+            car.thumbnail ||
+            (car.images_id && car.images_id[0]
+              ? car.images_id[0].thumbnail
+              : null),
+        };
+
+        return (
+          <div key={car.id} className="col-md-6 col-lg-4">
+            <CarCard
+              car={processedCar}
+              formatPrice={formatPrice}
+              formatMileage={formatMileage}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
